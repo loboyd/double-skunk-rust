030403b6979755566699b2980caacca3ff718daf
loboyd <logan.boyd@protonmail.com>
2020-11-28 03:03:06 -0500

    Remove `opponent` from `Client`; Add `opponent` functionality

--- src/bogus_opponent.rs
+++ src/bogus_opponent.rs
@@ -5,6 +5,7 @@
 
 //use crate::ui;
 use crate::opponent;
+use crate::card;
 
 pub struct BogusOpponent { }
 
@@ -12,11 +13,40 @@ pub struct BogusOpponent { }
 // (otherwise, find a better way to solve this problem)
 impl opponent::Opponent for BogusOpponent {
     fn determine_first_dealer(&self) -> bool {
-        true
+        false
     }
 
-    fn deal(&self) -> (Vec<u8>, u8) {
-        (vec![1, 2, 3, 4, 5, 6], 7)
+    fn deal(&self, dealer: bool) -> (Vec<card::Card>, card::Card) {
+        (vec![
+            card::Card::new(0),
+            card::Card::new(1),
+            card::Card::new(2),
+            card::Card::new(3),
+            card::Card::new(4),
+            card::Card::new(5)],
+        card::Card::new(6))
+    }
+
+    fn discard(&self, dealer: bool, hand: Vec<card::Card>, ind1: u8, ind2: u8)
+        -> (Vec<card::Card>, Option<Vec<card::Card>>) {
+
+        let mut new_hand = Vec::new();
+        // TODO: Write this in a better way
+        for (ind, card) in hand.iter().enumerate() {
+            if ind != ind1 as usize && ind != ind2 as usize {
+                new_hand.push(card);
+            }
+        }
+
+        if dealer {
+            (hand, Some(vec![
+                card::Card::new(0),
+                card::Card::new(1),
+                card::Card::new(2),
+                card::Card::new(3)]))
+        } else {
+            (hand, None)
+        }
     }
 }
 
